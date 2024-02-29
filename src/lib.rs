010bf79d932b6eb411078ba9c80c506360b96c69
miaomiao1992 <644284807@qq.com>
2024-02-29 10:33:58 +0800

    update

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
+
+#[cfg(not(windows))]
+include!("bindings_macos.rs");
+
+#[cfg(all(windows, target_arch = "x86_64"))]
+include!("bindings_windows_x64.rs");
+
+#[cfg(all(windows, target_arch = "x86"))]
+include!("bindings_windows_x86.rs");
